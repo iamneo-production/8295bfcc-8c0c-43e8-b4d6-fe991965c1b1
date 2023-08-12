57eb51711823300ba6d050bb662a76217a311b20
lallii <lalithasunkara714@gmail.com>
2023-08-12 10:07:05 +0000

    Language translator

---
+++ demo/src/App.js
@@ -0,0 +1,21 @@
+import Translate from "./components/Translate";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import SpecificLanguage from "./components/SpecificLanguage";
+import Languages from "./components/Languages";
+import "./App.css";
+import "semantic-ui-css/semantic.min.css";
+import React from "react";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/languages" element={<Languages />} />
+        <Route path="/" element={<Translate />} />
+        <Route path="/languages/:langCode" element={<SpecificLanguage />} />
+      </Routes>
+    </BrowserRouter>
+    
+  );
+}
