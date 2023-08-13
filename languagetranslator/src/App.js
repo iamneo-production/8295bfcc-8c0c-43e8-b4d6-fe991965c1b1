7814e60bb0b1f41f3b1fc5bf467cb9e1d41a7858
LakshmiYasaswi <yasaswikanamarlapudi@gmail.com>
2023-08-13 06:30:37 +0000

    comitted

---
+++ languagetranslator/src/App.js
@@ -0,0 +1,39 @@
+import Translate from "./components/Translate";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+
+
+
+import SpecificLanguage from "./components/SpecificLanguage";
+
+import Languages from "./components/Languages";
+
+import "./App.css";
+
+import "semantic-ui-css/semantic.min.css";
+
+
+
+
+export default function App() {
+
+  return (
+
+    <BrowserRouter>
+
+      <Routes>
+
+        <Route path="/languages" element={<Languages />} />
+
+        <Route path="/" element={<Translate />} />
+
+        <Route path="/languages/:langCode" element={<SpecificLanguage />} />
+
+      </Routes>
+
+    </BrowserRouter>
+
+  );
+
+}
